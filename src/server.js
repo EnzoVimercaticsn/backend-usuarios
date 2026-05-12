6a2ad456f1e1836a4d520409c111fb98edae05d3
EnzoVimercaticsn <enzo.vimercati@csn.com.br>
2026-05-12 16:47:21 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -3,7 +3,15 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: ['http://127.0.0.1:5501', 'http://localhost:5501', 'https://backend-usuarios.vercel.app'], // Allow local development and your Vercel domain
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes)
 
